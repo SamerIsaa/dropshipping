da1686d1e44f0ebb019874fc97fa5d5e9333ac55
Samer Isaa <samer.isaa1996@gmail.com>
2026-01-12 13:38:25 +0200

    Update CartItem.php

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -18,6 +18,7 @@ class CartItem extends Model
         return $this->belongsTo(Product::class);
     }
 
+    
     public function variant()
     {
         return $this->belongsTo(ProductVariant::class);
